7d684d9d24cc0bfb0abe0386c63d78a604f1a39c
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-05-06 15:33:42 -0500

    feat: manda mensajes con while

--- sockets/src/HiloCliente.java
+++ sockets/src/HiloCliente.java
@@ -1,6 +1,7 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Scanner;
 
 public class HiloCliente extends Thread  {
     private DatagramPacket paqueteEntrada;
@@ -10,22 +11,30 @@ public class HiloCliente extends Thread  {
         this.paqueteEntrada = paqueteEntrada;
     }
 
+
     public void run() {
+        Scanner scanner = new Scanner(System.in);
         try {
             //extraer la infomracion del paquete
+
             String mensajeRecibido = new String(paqueteEntrada.getData());
-            System.out.println("Mensaje recibido: => " + mensajeRecibido);
+            System.out.println("Mensaje recibido desde el cliente: => " + mensajeRecibido);
 
             //obtner direccion del cliente
             InetAddress direccionIp_cliente = paqueteEntrada.getAddress();
             int puerto_cliente = paqueteEntrada.getPort();
 
-            //crear un arreglo de bytes para enviar los datos del cliente
-            String mensajeSalida = "HOlaaa soy el como se llama";
-            byte[] bufferSalida = mensajeSalida.getBytes();
-            //crea un objeto para enviar los datos
-            DatagramPacket paqueteSalida = new DatagramPacket(bufferSalida, bufferSalida.length, direccionIp_cliente, puerto_cliente);
-            socket.send(paqueteSalida);
+            while(true) {
+                //crear un arreglo de bytes para enviar los datos del cliente
+                System.out.println("Ingresa el mesnaje desde el servidor: ");
+                String mensajeSalida = scanner.nextLine();
+                byte[] bufferSalida = mensajeSalida.getBytes();
+                //crea un objeto para enviar los datos
+                DatagramPacket paqueteSalida = new DatagramPacket(bufferSalida, bufferSalida.length, direccionIp_cliente, puerto_cliente);
+                socket.send(paqueteSalida);
+            }
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
