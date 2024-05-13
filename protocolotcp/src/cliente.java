1d17bfd1e1beb36de18428230c76b6495012cfdf
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-05-13 15:42:19 -0500

    coreecion

--- protocolotcp/src/cliente.java
+++ protocolotcp/src/cliente.java
@@ -11,21 +11,29 @@ public class cliente {
         int port = 5000;
         try {
 
-            Socket socket_client = new Socket("localhost", port);
+            Socket socket_client = new Socket("25.62.115.218", port);
             System.out.println("cliente conectado");
 
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(socket_client.getInputStream()));
+            PrintWriter salida = new PrintWriter(socket_client.getOutputStream(), true);
+            String datos_recibidos;
+            //leer los datos recibidos por el cliente almacendados en la nube
             while (true) {
-                BufferedReader entrada = new BufferedReader(new InputStreamReader(socket_client.getInputStream()));
-                PrintWriter salida = new PrintWriter(socket_client.getOutputStream(), true);
+                datos_recibidos = entrada.readLine();
+                System.out.println("El mensaje recibido del servidor es: " +  datos_recibidos);
 
                 //escribe los daots a enviar a salida
                 System.out.println("Ingrese mensaje al servidor => ");
                 String mensaje_enviar = scanner.nextLine();
                 salida.println((mensaje_enviar));
 
-                //leer los datos recibidos por el cliente almacendados en la nube
-                String datos_recibidos = entrada.readLine();
-                System.out.println("El mensaje recibido del servidor es: " +  datos_recibidos);
+                // Esperar la siguiente pregunta del servidor antes de permitir otra respuesta
+                String siguientePregunta = entrada.readLine();
+                if (siguientePregunta == null) {
+                    break;
+                }
+
+
             }
         } catch (IOException e) {
             e.printStackTrace();
